5919df3b6716c920cdecf7c3afb6e7783c970be8
bibao <quocbao2116@gmail.com>
2024-07-10 14:38:07 +0700

    done

---
+++ fire-base.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCmXA5A1FnQbimvuP03yT74V1aO90JDYtA",
+    authDomain: "bracelet-sell.firebaseapp.com",
+    projectId: "bracelet-sell",
+    storageBucket: "bracelet-sell.appspot.com",
+    messagingSenderId: "749485716327",
+    appId: "1:749485716327:web:297fc24b8c0d10d4d7b5c2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
